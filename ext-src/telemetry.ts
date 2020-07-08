ab6297399e2130b3ff4df46cf64a499048fb3cd8
Kenneth Auchenberg <kenneth@aucehnberg.dk>
2020-07-08 15:15:21 -0700

    Remove MixPanel

--- ext-src/telemetry.ts
+++ ext-src/telemetry.ts
@@ -1,11 +1,9 @@
-var Mixpanel = require('mixpanel');
 import * as vscode from 'vscode';
 const osName = require('os-name');
 const publicIp = require('public-ip');
 var Amplitude = require('amplitude');
 
 export class Telemetry {
-  client: any;
   amplitude: any;
   userId: string;
   ip: string;
@@ -26,11 +24,10 @@ export class Telemetry {
       return;
     }
 
-    if (this.client) {
+    if (this.amplitude) {
       return;
     }
 
-    this.client = Mixpanel.init('d0149f7b700b44a18fa53e2cab03b564');
     this.amplitude = new Amplitude('ab1883d0fa15e411b12ee8fdf476b952');
 
     let extension = vscode.extensions.getExtension('auchenberg.vscode-browser-preview');
@@ -38,16 +35,6 @@ export class Telemetry {
 
     // Store
     this.ip = await publicIp.v4();
-
-    // Mixpanel
-    this.client.people.set(this.userId, {
-      sessionId: vscode.env.sessionId,
-      language: vscode.env.language,
-      vscodeVersion: vscode.version,
-      platform: osName(),
-      version: extensionVersion,
-      ip: this.ip
-    });
 
     // Amplitude
     this.amplitude.identify({
@@ -73,9 +60,6 @@ export class Telemetry {
       distinct_id: this.userId,
       ip: this.ip
     };
-
-    // Mixpanel
-    this.client.track(eventName, data);
 
     // Amplitude
     this.amplitude.track({
